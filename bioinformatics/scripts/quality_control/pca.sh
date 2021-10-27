f56d51d1776aaa9108ad26830b544f4cb0a16438
sjellerstrand <81574558+sjellerstrand@users.noreply.github.com>
2021-10-27 17:28:57 +0200

    Delete bioinformatics/scripts directory

--- bioinformatics/scripts/quality_control/pca.sh
+++
@@ -1,9 +0,0 @@
-### Do a PCA
-## Prune variants
-plink --vcf $VCF_OUT.vcf.gz --double-id --allow-extra-chr --set-missing-var-ids @:# --indep-pairwise 50 10 0.1 --out $VCF_OUT;
-## Calculate Pricipal components
-plink --vcf $VCF_OUT.vcf.gz --double-id --allow-extra-chr --set-missing-var-ids @:# --extract $VCF_OUT.prune.in --pca 20 --out $VCF_OUT;
-
-### Plot Pricipal component 1 and 2
-Rscript $MAINDIR/scripts/radseq2020/quality_control/pca.r \
---args VCF_OUT=$VCF_OUT DATA=$DATA METADATA=$METADATA;
